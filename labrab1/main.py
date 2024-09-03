0b790e8dc21b38255aa1c2e51fb5b244a6b796d0
Aleksey <11_alx_11@ro.ru>
2024-09-03 12:51:19 +0300

    dev-09.03-12:51

--- labrab1/main.py
+++ labrab1/main.py
@@ -3,14 +3,19 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# Вариант №3
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Функция для тестирования:
+# y = a * sin( sin( b * x ) ) + d
+
+#   |-----------------------------------|
+#   |   a   b    d   Кол-во входов ИНС  |
+#   |   3	7	0.3	        5           |
+#   |-----------------------------------|
+
+def main():
+    print("Hi")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
