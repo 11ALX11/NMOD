9b872b3a7785264115039a78eda894603c92cc07
Aleksey <11_alx_11@ro.ru>
2024-09-06 14:42:52 +0300

    working on stage 4

--- labrab1/main.py
+++ labrab1/main.py
@@ -17,7 +17,7 @@ from random import random
 #   y = 3 * sin( sin( 7 * x ) ) + 0.3
 #   Период функции = (2*Pi)/7 = 0.8975979010256552
 INPUT_DATA_MAX_STEP = 0.07
-INPUT_DATA_MIN_STEP = 0.01
+INPUT_DATA_MIN_STEP = 0.02
 
 #   Графическая интерпретация НС на 5 входов
 #   x1 --> () w1 --\
@@ -27,8 +27,8 @@ INPUT_DATA_MIN_STEP = 0.01
 #   x5 --> () w5 --/
 
 
-ALPHA = 0.3         # шаг обучения 0 < a < 1
-E_OPTIMAL = 1e-4    # минимальная среднеквадратичная ошибка НС
+ALPHA = 0.00005     # шаг обучения 0 < a < 1
+E_OPTIMAL = 20.     # минимальная среднеквадратичная ошибка НС
 
 NN_WIDTH = 5        # количество входных образов (Кол-во входов ИНС)
 
@@ -46,10 +46,14 @@ input_values    = []    # x's (иксы)
 data_values     = []    # y's (игрики)
 NN_inout_values = []    # значения для входа в НС
 
-weights         = []    # веса НС -0.5 < w < 0.5
-error_current   = 1.    # текущая ошибка НС
-theta           = 0     # порог НС;
-                        # используется при вычислении выходного значения НС
+weights         = []                # веса НС -0.5 < w < 0.5
+error_current   = E_OPTIMAL + 1     # текущая ошибка НС
+theta           = 0                 # порог НС;
+                                    # используется при вычислении выходного значения НС
+
+
+def clamp(n, smallest, largest): return max(smallest, min(n, largest))
+
 
 # подготовка, инициализация данных
 def init_data():
@@ -121,7 +125,7 @@ def print_stage3():
         inputs = ", ".join(f"y{j+1}({in_value[j-i]})" for j in range(i, i + NN_WIDTH))
         output = f"y{i + NN_WIDTH + 1}({out_value})"
 
-        if i + NN_WIDTH <= LEARN_DATA_AMOUNT - 1:
+        if i + NN_WIDTH < LEARN_DATA_AMOUNT:
             print(f"{inputs} -> {output}")
         else:
             print("\ntest_data (y -> original value, y’ -> model output value):")
@@ -133,29 +137,38 @@ def print_stage3():
 # возвращает y`, найденный с помощью НС
 def get_y_NN(in_value: list) -> float:
     w_sum = 0.
+    #print() # TODO: debug cleanup
 
     i = 0
     while i < NN_WIDTH:
         w_sum += weights[i] * in_value[i]
+
+        #print(f"w_sum = {w_sum}, w{i+1} = {weights[i]}, x = {in_value[i]}")# TODO: debug cleanup
         i += 1
 
+    #print(f"theta = {theta}")# TODO: debug cleanup
     return w_sum - theta
 
 # возвращает ошибку
 # @param y - значение НС
 # @param e - эталонное значение
-def get_error(y, e) -> float:
-    return 0.5 * ((y - e)**2)
+def get_error(y, e) -> float: return 0.5 * ((y - e) ** 2)
 
 # изменяет веса и порог НС
+# @param y - значение НС
+# @param e - эталонное значение
 def mutate_weights(y, e, in_values: list):
     i = 0
     while i < NN_WIDTH:
-        weights[i] = weights[i] - ALPHA * (y - e) * in_values[i]
+        weights[i] = clamp(weights[i] - ALPHA * (y - e) * in_values[i],
+                           WEIGHTS_RANGE_LOW,
+                           WEIGHTS_RANGE_TOP)
         i += 1
 
     global theta
-    theta = theta - ALPHA * (y - e)
+    theta = clamp(theta - ALPHA * (y - e),
+                           WEIGHTS_RANGE_LOW,
+                           WEIGHTS_RANGE_TOP)
 
 # подготовить начальные значения для весов
 def init_weights():
@@ -179,23 +192,26 @@ def train():
 
     while error_current > E_OPTIMAL:
         print(f"\nGeneration №{generation_counter}")
+        old_weights_debug = weights.copy()
         error_current = 0.
+        train_loss = 0
+        inout_values = NN_inout_values.copy()
 
         i = 0
-        while i < DATA_AMOUNT:
-            inout_value = NN_inout_values[i]
+        while i < DATA_AMOUNT - NN_WIDTH:
+            inout_value = inout_values[i]
             in_value = inout_value[0]
+            # print(f"i - {i}; in_value - {in_value}") # TODO: debug cleanup
             out_value = inout_value[1]
 
             y = get_y_NN(in_value)
+            #print(f"y`{i+NN_WIDTH+1} = {y}, y{i+NN_WIDTH+1} = {out_value}") # TODO: debug cleanup
 
-            if i + NN_WIDTH <= LEARN_DATA_AMOUNT - 1:
+            if i + NN_WIDTH < LEARN_DATA_AMOUNT:
                 # тренировка
                 mutate_weights(y, out_value, in_value)
             else:
                 # тестирование
-                out_value = y
-
                 j = 1
                 new_in_value = []
                 while j < NN_WIDTH:
@@ -204,18 +220,26 @@ def train():
                         new_in_value.append(y)
                     j += 1
 
-                NN_inout_values.append([new_in_value, 0])
+                inout_values.append([new_in_value, 0])
+
+                out_value = data_values[i + NN_WIDTH] # переопределяем для проверки
 
             error = get_error(y, out_value)
             error_current += error
 
+            if i + NN_WIDTH - 1 == LEARN_DATA_AMOUNT:
+                train_loss = error_current
+
             i += 1
 
-        print(f"test_loss: {error_current}")
+        print(f"train_loss: {train_loss}\ttest_loss: {error_current}")
         if error_current > E_OPTIMAL:
             print(f"test_loss > {E_OPTIMAL} -> continue training")
         else:
             print(f"test_loss < {E_OPTIMAL} -> stop training")
+
+
+        print(f"old_weights {old_weights_debug} -> \nnew_weights {weights}") # TODO: debug cleanup
 
         generation_counter += 1
 
